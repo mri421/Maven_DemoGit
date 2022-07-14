e286f92073ce4d00cb2fa06bb15a15fd60feb58d
SYSTEM <mridulkhandelwal26@gmail.com>
2022-07-14 12:33:08 +0530

    second commit

--- Maven_Demogiti/src/main/java/com/lti/entity/UserAapp.java
+++ Maven_Demogiti/src/main/java/com/lti/entity/UserAapp.java
@@ -3,8 +3,7 @@ package com.lti.entity;
 public class UserAapp {
 
 	public static void main(String[] args) {
-		Calculator c = new Calculator();
-		int r = c.add(100,300);
-		System.out.println(r);
+		
+		System.out.println("Student");
 	}
 }
